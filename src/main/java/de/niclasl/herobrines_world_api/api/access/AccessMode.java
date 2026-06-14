edc2f14d3ccb5b199eef8d5fc75d19f4bac44949
Niclas-31 <niclasloeffler3105@gmail.com>
2026-06-14 12:25:49 +0200

    Initial commit

--- src/main/java/de/niclasl/herobrines_world_api/api/access/AccessMode.java
+++ src/main/java/de/niclasl/herobrines_world_api/api/access/AccessMode.java
@@ -1,12 +1,10 @@
 package de.niclasl.herobrines_world_api.api.access;
 
 import com.mojang.serialization.Codec;
-import de.niclasl.herobrines_world_api.annotation.Experimental;
 import de.niclasl.herobrines_world_api.registry.HWRegistries;
 import net.minecraft.resources.Identifier;
 import org.jetbrains.annotations.NotNull;
 
-@Experimental
 public interface AccessMode {
     Codec<AccessMode> CODEC =
             Identifier.CODEC.xmap(
