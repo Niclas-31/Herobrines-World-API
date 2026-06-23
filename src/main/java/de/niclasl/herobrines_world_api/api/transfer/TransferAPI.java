15fcd93955ec151cf0837b5909e426866039149b
Niclas-31 <niclasloeffler3105@gmail.com>
2026-06-23 16:09:00 +0200

    Initial commit

--- src/main/java/de/niclasl/herobrines_world_api/api/transfer/TransferAPI.java
+++ src/main/java/de/niclasl/herobrines_world_api/api/transfer/TransferAPI.java
@@ -2,14 +2,18 @@ package de.niclasl.herobrines_world_api.api.transfer;
 
 import de.niclasl.herobrines_world_api.api.transfer.resolver.InventoryResolver;
 
-public final class TransferAPI {
-    private static InventoryResolver resolver;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-    public static void setResolver(InventoryResolver resolver) {
-        TransferAPI.resolver = resolver;
+public class TransferAPI {
+    private static final List<InventoryResolver> RESOLVERS = new ArrayList<>();
+
+    public static void registerResolver(InventoryResolver resolver) {
+        RESOLVERS.add(resolver);
     }
 
-    public static InventoryResolver resolver() {
-        return resolver;
+    public static List<InventoryResolver> getResolvers() {
+        return Collections.unmodifiableList(RESOLVERS);
     }
 }
